29ce9b8d3f05f522469b4ac3f5774099611c152d
Nilesh Agarwal <nick.aggarwal@gmail.com>
2023-10-26 22:39:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ class InferlessPythonModel:
     def initialize(self):
         print("New Changes are comming")
         self.pipe = StableDiffusionPipeline.from_pretrained(
-            "runwayml/stable-diffusion-v1-5",
+            "stabilityai/stable-diffusion-2-1",
             use_safetensors=True,
             torch_dtype=torch.float16,
             device_map='auto'
@@ -21,7 +21,7 @@ class InferlessPythonModel:
         buff = BytesIO()
         image.save(buff, format="JPEG")
         img_str = base64.b64encode(buff.getvalue()).decode()
-        return { "label_1" : 0.398 , "label_2" : 0.563, "label_3" : 0.434 }
+        return { "generated_image_base64" : img_str }
         
     def finalize(self):
         self.pipe = None
