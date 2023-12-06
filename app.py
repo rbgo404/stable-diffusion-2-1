412222f39d48d701a4c169d0b9d2026e51cbe567
Nilesh Agarwal <nick.aggarwal@gmail.com>
2023-12-06 13:29:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,21 @@ from diffusers import StableDiffusionPipeline
 import torch
 from io import BytesIO
 import base64
-
+from huggingface_hub import snapshot_download
+import os
 
 class InferlessPythonModel:
     def initialize(self):
         print("New Changes are comming on 5 dec")
-        self.pipe = StableDiffusionPipeline.from_pretrained(
+        __location__ = os.path.real path(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        __parent_location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__), "..") )
+        snapshot_download(
             "stabilityai/stable-diffusion-2-1",
+            local_dir=__parent_location__
+        )
+        
+        self.pipe = StableDiffusionPipeline.from_pretrained(
+            __parent_location__,
             use_safetensors=True,
             torch_dtype=torch.float16,
             device_map='auto'
