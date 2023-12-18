f2de242897d11e93369b36dba52f52a53e1be622
Nilesh Agarwal <nick.aggarwal@gmail.com>
2023-12-18 16:35:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 class InferlessPythonModel:
     def initialize(self):
-        print("Hello World 7")
+        print("Hello World 8")
         self.pipe = StableDiffusionPipeline.from_pretrained(
             "stability/stable-diffusion-2-1",
             use_safetensors=True,
